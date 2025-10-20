ad9a4bdc5b6771f91878ae6000061515c0332268
masdrive <masdrivetrackermail@gmail.com>
2025-10-20 14:55:19 +0100

    Topfold rename

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBKgIkF4LSFNE4FHgdB4DeP2dPi8BTcLpE",
-  authDomain: "masdrivefrontend.firebaseapp.com",
-  projectId: "masdrivefrontend",
-  storageBucket: "masdrivefrontend.firebasestorage.app",
-  messagingSenderId: "252737770505",
-  appId: "1:252737770505:web:233a89da214c31e50214d8",
-  measurementId: "G-HENM6C3LMD"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
